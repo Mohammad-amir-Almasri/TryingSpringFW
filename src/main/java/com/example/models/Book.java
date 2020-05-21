853a1509427cb4c2899e4d0010b18bae2a36685f
YoussefKafa <48362996+YoussefKafa@users.noreply.github.com>
2020-05-22 00:22:50 +0300

    updatePackages

--- src/main/java/com/example/models/Book.java
+++ src/main/java/com/example/models/Book.java
@@ -1,15 +1,13 @@
-package Models;
+package com.example.models;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
 @Entity
 public class Book {
 	@Id
 	@GeneratedValue
 	private int id;
-	
 	private String name;
 	private double price;
 	
