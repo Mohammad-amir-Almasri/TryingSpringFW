9b299b06a3fe1b9bfae11ad43668634e4d4a7d04
AmirThePrince <almasriamir98@gmail.com>
2020-05-21 23:39:04 +0300

    Create Book.java
    commit Book class

---
+++ src/main/java/Models/Book.java
@@ -0,0 +1,40 @@
+package Models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String name;
+	private double price;
+	
+	Book(int i, String n, double p){
+		id =i;
+		name=n;
+		price=p;
+	}
+	
+	void set(int i, String n, double p) {
+		id =i;
+		name=n;
+		price=p;
+	}
+	
+	int getId() {
+		return id;
+	}
+	
+	double getPrice() {
+		return price;
+	}
+	
+	String getName() {
+		return name;
+	}
+
+}
